4bee979cca10a919ea029aacefcb853db9e17400
Devendra116 <chauhandevendra116@gmail.com>
2023-06-15 00:17:51 +0530

    add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-white">
+          <h1 className="text-4xl font-bold mb-6 text-center">Page Not Found</h1>
+      </div>
+    </>
+  );
+};
+
+
+
